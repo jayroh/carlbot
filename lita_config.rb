0bacc9e79e903b4dbfa1634dbf6026ae4193b8a9
Joel Oliveira <joel.oliveira@gmail.com>
2015-01-05 13:09:35 -0500

    Cleanup config

--- lita_config.rb
+++ lita_config.rb
@@ -1,15 +1,9 @@
 Lita.configure do |config|
   config.robot.adapter = :slack
   config.adapters.slack.token = ENV['SLACK_TOKEN']
-  config.robot.admins = [
-    'U039BR581',
-    'U039BS2KD'
-  ]
-
-  # The severity of messages to log. Options are:
-  # :debug, :info, :warn, :error, :fatal
-  # Messages at the selected level and above will be logged.
-  config.robot.log_level = :debug
+  config.robot.name = 'carl'
+  config.robot.admins = ['U039BR581', 'U039BS2KD']
+  config.robot.log_level = :info # :debug, :info, :warn, :error, :fatal
 
   config.redis[:url] = ENV['REDISTOGO_URL']
   config.http.port = ENV['PORT']
